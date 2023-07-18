7cf1c0bc3f3f7953023e5c102b3043a73d77ce5b
Agustin Alsop <agustinalsop@gmail.com>
2023-07-18 18:53:47 -0300

    Agrego el scrip procesar.sh

---
+++ procesar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Scrip: Procesar.sh
+# Funcion:
+# Recorta imágenes a una resolución de 512*512. Solamente procesa aquellas imágenes
+# que tengan nombres de personas válidos. Entiéndase por nombres de personas válidos a
+# cualquier combinación de palabras que empiecen con un letra mayúscula y sigan por minúsculas.
+
