8aaa82164549e40b04926b4e3ec537589fd93ff3
Agustin Alsop <agustinalsop@gmail.com>
2023-07-18 20:18:38 -0300

    cargo modificaciones ya funcionales del scrip

--- procesar.sh
+++ procesar.sh
@@ -5,3 +5,9 @@
 # que tengan nombres de personas válidos. Entiéndase por nombres de personas válidos a
 # cualquier combinación de palabras que empiecen con un letra mayúscula y sigan por minúsculas.
 
+imagen=$1
+nombre_salida=$2
+
+convert $imagen -crop 512x512+100+100 "$nombre_salida"
+
+exit 0
