c9a5956dda584236d86846336bbfd4aed2d2b12c
AleP <alejandro.peralta07@gmail.com>
2023-07-20 20:31:31 -0300

    Se agregó rutas, archivos y directorios- Modificación de asignación de nombres a las imágenes

--- generar.sh
+++ generar.sh
@@ -1,56 +1,72 @@
 #!/bin/bash
 
+#Rutas, archivos y directorios
+RUTA_UBICACION=$(pwd)
+DIRECT_SALIDA="$RUTA_UBICACION/imagenes_generadas"
+NOMBRES_TXT="nombres.txt"
+
+#Expresiones regulares
+REGEXP_NOMBRE="^[A-Za-z ]+"
+REGEXP_NUMERO="^[1-9][0-9]*$"
+
 echo "Ingrese la cantidad de imagenes que desea descargar: "
-read -r IMAGENES
+read -r CANTIDAD_IMAGENES
 
 # Validamos si el valor ingresado por el usuario es un nùmero entero positivo
-
-if echo "$IMAGENES" | grep -qP '^[1-9][0-9]*$'; then  #priemr digito rango del 1-9, luego cualquier numero entre 0 y 9
-  echo "Número válido de imagenes"
+if [[ "$CANTIDAD_IMAGENES" =~ $REGEXP_NUMERO ]]
+then
+	echo "Número válido de imagenes"
 else
-  echo "Debe ingresar un nùmero entero positivo"
-  exit 1
-fi
-#Solicitamos el ingreso del nombre de la carpeta destino
-
-echo "Ingrese el nombre de la carpeta donde desea guardar las imágenes"
-read -r DIRECTORIOSALIDA
-
-#validamos el ingreso
-if [[ -z  "$DIRECTORIOSALIDA" ]]; then
-  echo "Error - Debe proporcionar un nombre"
-  exit 1
+	echo "Debe ingresar un nùmero entero positivo"
+	exit 1
 fi
 
-# Creamos el directorio salida , el -p nos crea los directorios padres si no existen
-mkdir -p "$DIRECTORIOSALIDA"
+# Creamos el directorio salida
+mkdir -p $DIRECT_SALIDA   #-p , si ya existe, no lo crea
+echo "Ingrese el nombre del archivo comprimido"
+read -r ARCHIVO_COMPRIMIDO
 
-NOMBRES_ALEATORIOS=($(curl -s https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv | grep -oE '[[:alpha:]]+')) #filtro de nombres 
+NOMBRES_TOTALES=$(curl -s https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv | grep -oE "$REGEXP_NOMBRE")
 
+echo "$NOMBRES_TOTALES" > $NOMBRES_TXT
 
-# Bucle for para generar img
-for ((i=0; i<IMAGENES; i++)) #este bucle for se ejecuta IMAGENES veces , lo arrancamos en 0 y aumenta 1 por cada iteracion hasta i-1
+# Generamos las imágenes según la cantidad ingresada por el usuario
+for ((i=0; i < CANTIDAD_IMAGENES; i++))
 do
-  INDICE_ALEATORIO=$((RANDOM % ${#NOMBRES_ALEATORIOS[@]}))
+ 	NOMBRE_ALEATORIO=$(shuf -n 1 $NOMBRES_TXT)
+ 	NOMBRE_ARCHIVO="${NOMBRE_ALEATORIO}.jpg"
 
-  NOMBRE_ARCHIVO="${NOMBRES_ALEATORIOS[$INDICE_ALEATORIO]}_$i.jpg"  
+	# Descargar imagen desde el servicio web
+  	curl -o "$DIRECT_SALIDA/$NOMBRE_ARCHIVO" -L "https://source.unsplash.com/random/900%C3%97700/?person."  #el curl , por defecto descarga y guarda las img en "imagenes", con el -o podemos especificar>
 
- # Descargar imagen desde el servicio web
-  curl -o "$DIRECTORIOSALIDA/$NOMBRE_ARCHIVO" -L "https://source.unsplash.com/random/900%C3%97700/?person."  #el curl , por defecto descarga y guarda las img en "imagenes", con el -o podemos especificarle una ruta
-  
-  # Esperar 5 segundo entre descargas
-  sleep 2
-  #con awk elegimos la primer columna, si no nos muestra la ruta del archivo (puede estar o no)
-  SUMA_VERIFICACION=$(md5sum "$DIRECTORIOSALIDA/$NOMBRE_ARCHIVO") # | awk '{ print $1 }')
+ 	 # Esperar 2 segundos entre descargas
+  	sleep 2
 
-  # SUMA de verificacion en un archivo de texto
-  echo "$SUMA_VERIFICACION  $NOMBRE_ARCHIVO" >> "$DIRECTORIOSALIDA/sumas_verificacion.txt" 
+  	# suma de verificación
+#  	SUMA_VERIFICACION=$(md5sum "$DIRECT_SALIDA/$NOMBRE_ARCHIVO")
 
+  	# SUMA de verificacion en un archivo de texto
+#  	echo "$SUMA_VERIFICACION" >> "${DIRECT_SALIDA}/${ARCHIVO_COMPRIMIDO}.txt"
 done
 
+rm $NOMBRES_TXT
+
 #comprimir las imagenes
-COMPRIMIR="$DIRECTORIOSALIDA"/imagenes.zip
-zip  "$COMPRIMIR" "$DIRECTORIOSALIDA"/*.jpg
+COMPRIMIR="${DIRECT_SALIDA}/${ARCHIVO_COMPRIMIDO}.zip"
+echo $COMPRIMIR
+
+cd $DIRECT_SALIDA
+zip "${ARCHIVO_COMPRIMIDO}.zip" ./*.jpg
+cd ..
+
+rm "$DIRECT_SALIDA"/*.jpg
+
+# suma de verificación
+SUMA_VERIFICACION=$(md5sum $COMPRIMIR)
+
+# SUMA de verificacion en un archivo de texto
+echo "$SUMA_VERIFICACION" >> "${DIRECT_SALIDA}/${ARCHIVO_COMPRIMIDO}.txt"
+
 
 echo "Las imágenes fueron comprimidas con exito"
-
+echo $COMPRIMIR
