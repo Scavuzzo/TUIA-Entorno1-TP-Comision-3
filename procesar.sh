7fd4e42af74306138e9a214bf1f275ad8d30d16e
Agustin Alsop <agustinalsop@gmail.com>
2023-07-20 13:39:10 -0300

    Modificacion del archivo procesar. primera version funcional

--- procesar.sh
+++ procesar.sh
@@ -5,9 +5,26 @@
 # que tengan nombres de personas válidos. Entiéndase por nombres de personas válidos a
 # cualquier combinación de palabras que empiecen con un letra mayúscula y sigan por minúsculas.
 
-imagen=$1
-nombre_salida=$2
+# Creacion de las variables
+CARPETA_ORIGEN="/home/alsop-agustin/Desktop/descomprimidos"
+CARPETA_DESTINO="/home/alsop-agustin/Desktop/procesados"
+EXPRESION="^[A-Z][a-z]+[ ][A-Z][a-z]+"
 
-convert $imagen -crop 512x512+100+100 "$nombre_salida"
+# Validacion de existencia de imagenes en la carpeta
+cd $CARPETA_ORIGEN
+LISTA_ARCHIVOS=$(find . -name "*.jpeg")
+cd ..
+
+[ -z "$LISTA_ARCHIVOS" ] && echo "No se encontraron fotos en el directorio" && exit 1
+
+# Proceso de modificacion de las imagenes
+for IMAGEN in "$CARPETA_ORIGEN"/*
+do
+        if [[ $IMAGEN == *.jpg || $IMAGEN == *.png || $IMAGEN == *.jpeg ]]
+        then
+                NOMBRE=$(basename "$IMAGEN")
+                [[ $NOMBRE =~ $EXPRESION ]] && convert "$IMAGEN" -resize 512x512 "$CARPETA_DESTINO/$NOMBRE"
+        fi
+done
 
 exit 0
